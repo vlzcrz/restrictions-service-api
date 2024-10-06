7623317dd23e0dc5e0ee3e64b3b2bb793cd64295
vlzcrz <velizcruzv@gmail.com>
2024-10-06 12:17:03 -0300

    Cambio en el uso de las credenciales de Firebase (Firestore) de archivo .json a variables de entorno especificado en .env.template.deploy

--- src/firestore/firestore.module.ts
+++ src/firestore/firestore.module.ts
@@ -1,16 +1,21 @@
 import { Module, Global } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import { Firestore } from '@google-cloud/firestore';
-import * as serviceAccount from '../config/restriction-service-ads-firebase-adminsdk.json';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
+  imports: [ConfigModule.forRoot()],
   providers: [
     {
       provide: Firestore,
       useFactory: () => {
         admin.initializeApp({
-          credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+          credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+          }),
         });
         return admin.firestore();
       },
